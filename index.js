568966e4c98322f18a6c2138c677fbf063712e86
niefarrell <farrellnathaniel2008@gmail.com>
2023-07-03 23:43:06 +0700

    berger menu error

---
+++ index.js
@@ -0,0 +1,13 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function ( ) {
+    nav.classList.toggle('slide');
+});
+
+
+
+
+
+
+
